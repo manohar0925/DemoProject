898e5a8f56931bdfa8bf7170bab0f64978f87442
manohar0925 <125366122+manohar0925@users.noreply.github.com>
2023-02-16 16:04:59 +0530

    Update views.py

--- DemoApp1/views.py
+++ DemoApp1/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def f1(manu):
-    return HttpResponse('<h1> HELLO USER GOOD AFTERNOON***</H1><HR/><BR/>')
+    return HttpResponse('<h1> ***HELLO USER GOOD AFTERNOON***</H1><HR/><BR/>')
 def f2(manu):
     return HttpResponse('<h2>😁 NAMASTE GURUGARU🌚</H2><HR/>')
